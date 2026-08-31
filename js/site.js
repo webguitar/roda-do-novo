8156a5f14151c996d47eeae20f1d35cd769802bc
webguitar <felipe.cambero@gmail.com>
2026-08-31 08:43:10 -0300

    Ativa o monitoramento de visitas via GoatCounter

--- js/site.js
+++ js/site.js
@@ -12,3 +12,28 @@ if (toggle && nav) {
 
 const year = document.querySelector("[data-year]");
 if (year) year.textContent = new Date().getFullYear();
+
+window.RODA_ANALYTICS = {
+  goatcounter: "roda-do-novo",
+  ga4: ""
+};
+
+(function startAnalytics(cfg) {
+  if (cfg.goatcounter) {
+    const s = document.createElement("script");
+    s.async = true;
+    s.src = "https://gc.zgo.at/count.js";
+    s.dataset.goatcounter = "https://" + cfg.goatcounter + ".goatcounter.com/count";
+    document.head.appendChild(s);
+  }
+  if (cfg.ga4 && /^G-[A-Z0-9]+$/.test(cfg.ga4)) {
+    const g = document.createElement("script");
+    g.async = true;
+    g.src = "https://www.googletagmanager.com/gtag/js?id=" + cfg.ga4;
+    document.head.appendChild(g);
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag("js", new Date());
+    gtag("config", cfg.ga4, { anonymize_ip: true });
+  }
+})(window.RODA_ANALYTICS);
